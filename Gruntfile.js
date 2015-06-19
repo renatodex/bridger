8516da0ed32de63c926b8d96d7b91e7b2155cbfe
Renato Alves <renatodex@gmail.com>
2015-06-19 19:39:49 -0300

    Default task now updates dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Task defining.
-  grunt.registerTask('default', 'jasmine');
   grunt.registerTask('build', ['concat', 'uglify']);
+  grunt.registerTask('default', ['build', 'jasmine']);
 };
